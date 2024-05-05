ce7479250d565e3309a8c034328962b1c1624c34
Jfoti64 <48845911+Jfoti64@users.noreply.github.com>
2024-05-04 20:10:41 -0500

    Simplify styles and change title

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,7 +2,7 @@ function Card({ handleClick, data }) {
   return (
     <button className="card" onClick={() => handleClick(data)}>
       <img src={data.imgSrc} alt="" />
-      {data.text}
+      <p>{data.text}</p>
     </button>
   );
 }
